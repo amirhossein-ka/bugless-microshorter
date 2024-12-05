13d88ae3552b4a16891282709e7a16e9bbbabe3a
amirhossein-ka <amirhossein.karimi9@gmail.com>
2024-12-05 12:48:15 +0330

    add config parser

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	GatewayConfig GatewayConfig `json:"gateway"`
 }
@@ -8,4 +13,14 @@ type GatewayConfig struct {
 	ListenPort     int    `json:"listen_port"`
 	ShortenerHost  string `json:"shortener_host"`
 	ShortenertPort int    `json:"shortener_port"`
+}
+
+func Parse(cfg *Config, path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return json.NewDecoder(file).Decode(cfg)
 }
