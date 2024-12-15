5d77f8dcbf314597c5c86df5f341df20c7fd0961
amirhossein-ka <amirhossein.karimi9@gmail.com>
2024-12-15 11:59:24 +0330

    Add rpc types

--- internal/rpcm/rpcm.go
+++ internal/rpcm/rpcm.go
@@ -1,3 +1,24 @@
-// rpcm pacakge is for models (structs) that is used for
+// Package rpcm is for models (structs) that is used for
 // net/rpc communication
 package rpcm
+
+type Args struct {
+	Keys []string
+}
+
+type Reply struct {
+	Results []string
+}
+
+// ShortenerService interface is intended to used like this in server side:
+//
+// var _ rpcm.ShortenerService = (*Shortener)(nil)
+type ShortenerService interface {
+	NewUrl(args *Args, reply *Reply) error
+	GetUrl(args *Args, reply *Reply) error
+}
+
+const (
+	NewUrl = "Shortener.NewUrl"
+	GetUrl = "Shortener.GetUrl"
+)
